835291327d01b1807b6cb85be8d19c1ed75d4dcd
Peyton Bechard <pbechard@my.yorku.ca>
2022-08-14 20:09:19 +0800

    Completes tutorial

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       mount: true,
       ignoreProp: 0,
       seed: 40,
+      showErrorComponent: false,
     };
 
     this.mountCounter = () => this.setState({ mount: true });
@@ -17,6 +18,8 @@ class App extends Component {
     this.ignoreProp = () => this.setState({ ignoreProp: Math.random() });
     this.seedGenerator = () =>
       this.setState({ seed: Number.parseInt(Math.random() * 100) });
+    this.toggleError = () =>
+      this.setState({ showErrorComponent: !this.state.showErrorComponent });
   }
 
   render() {
@@ -30,8 +33,13 @@ class App extends Component {
         </button>
         <button onClick={this.ignoreProp}>Ignore Prop</button>
         <button onClick={this.seedGenerator}>Generate Seed</button>
+        <button onClick={this.toggleError}>Toggle Error Component</button>
         {this.state.mount && (
-          <Counter ignoreProp={this.state.ignoreProp} seed={this.state.seed} />
+          <Counter
+            ignoreProp={this.state.ignoreProp}
+            seed={this.state.seed}
+            showErrorComponent={this.state.showErrorComponent}
+          />
         )}
       </div>
     );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+const ErrorComponent = () => <div></div>;
+// const ErrorComponent = () => <div>{props.ignore}</div>;
+
 export class counter extends Component {
   constructor(props) {
     console.log('Constructor');
@@ -8,6 +11,7 @@ export class counter extends Component {
     this.state = {
       counter: 0,
       seed: 0,
+      initializing: true,
     };
 
     this.increment = () => this.setState({ counter: this.state.counter + 1 });
@@ -26,6 +30,7 @@ export class counter extends Component {
 
   componentDidMount() {
     console.log('Component did mount');
+    setTimeout(() => this.setState({ initializing: false }), 500);
     console.log('-------------------');
   }
 
@@ -35,20 +40,39 @@ export class counter extends Component {
       this.props.ignoreProp !== nextProps.ignoreProp
     ) {
       console.log('Should component update - do not render');
+      console.log('-------------------');
       return false;
     }
     console.log('Should component update - render');
+    console.log('-------------------');
+
     return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('Get snapshot before update');
+    return null;
   }
 
   render() {
     console.log('Render');
+
+    if (this.props.showErrorComponent && this.state.error) {
+      return (
+        <div>We have encountered an error: {this.state.error.message}</div>
+      );
+    }
+
+    if (this.state.initializing) {
+      return <div>Please wait while we gather our resources...</div>;
+    }
 
     return (
       <div>
         <button onClick={this.increment}>Increment</button>
         <button onClick={this.decrement}>Decrement</button>
         <div className='counter'>Counter: {this.state.counter}</div>
+        {this.props.showErrorComponent && <ErrorComponent />}
       </div>
     );
   }
@@ -61,6 +85,11 @@ export class counter extends Component {
   componentWillUnmount() {
     console.log('Component will unmount');
     console.log('-------------------');
+  }
+
+  componentDidCatch(error, info) {
+    console.log('Component did catch');
+    this.setState({ error, info });
   }
 }
 
